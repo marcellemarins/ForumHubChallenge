9097268d349daf04ada0520f30ebda9acfa0e64f
Marcelle Marins <marcellemarins123@gmail.com>
2024-07-12 17:58:39 -0300

    1

---
+++ src/test/java/com/marcelle/ForumHubChallenge/ForumHubChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelle.ForumHubChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
